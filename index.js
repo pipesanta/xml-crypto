24445e9e661c09cc2d7e79b9afd67c8067e792ca
pipesanta <juansantag95@gmail.com>
2019-11-06 19:06:22 -0500

    c14n exported

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var select = require('xpath').select
 
-module.exports = require('./lib/signed-xml')
+module.exports = require('./lib/signed-xml');
+module.exports.C14N = require("./lib/c14n-canonicalization");
 module.exports.xpath = function(node, xpath) {
   return select(xpath, node)
 }
